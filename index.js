58311c673c4ab9414c25e0e08f1c48c90fc73dbc
Arindam Sarkar <arindam@73bit.com>
2025-07-10 12:32:13 +0530

    cx

--- index.js
+++ index.js
@@ -27,5 +27,5 @@ app.use(cors(corsOpts));
 app.use("/", routes);
 
 app.listen(port, () => {
-  console.log("Server started on port 3000" + port);
+  console.log("Server started on port" + port);
 });
